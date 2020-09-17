8fc366d5744039239406fb558b35b88d88850607
DreamEchoo100 <maze6572198@gmail.com>
2020-09-17 16:25:17 +0200

    Upload&update...

---
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/The Burger Builder (Basic Version)/Logo/Logo..js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const logo = (props) => (
+	
+);
+
+export default logo;
--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/The Burger Builder (Basic Version)/components/Layout/Layout.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/The Burger Builder (Basic Version)/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React  from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
 	<Aux>
+		<Toolbar />
 		<div> Toolbar, SlideDrawer, Backdrop</div>
 		<main className={classes.Content}>
 			{props.children}
