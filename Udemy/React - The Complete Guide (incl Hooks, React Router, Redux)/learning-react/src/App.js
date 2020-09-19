f6a79ad50ccaf298d9c69a50687d62595d4dee81
DreamEchoo100 <maze6572198@gmail.com>
2020-09-19 19:57:14 +0200

    Uplaod&Update...

--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/App.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 // import Tutorial1 from "./Tutorial1/Tutorial1";
-import MyBurger1 from "./The Burger Builder (Basic Version)/MyBurger1";
+// import MyBurger1 from "./The Burger Builder (Basic Version)/MyBurger1";
+import Tutorial2 from "./Tutorial2 - Reaching out to the Web (Http - Ajax)/Tutorial2";
 
 class App extends Component {
   render() {
-    return (
-    	<MyBurger1 />
-    );
+    return <Tutorial2 />;
   }
 }
 
