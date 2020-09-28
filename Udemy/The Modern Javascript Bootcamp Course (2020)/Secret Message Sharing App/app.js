8a12a68f05d133429db89f5d6d84975900a1efb9
DreamEchoo100 <maze6572198@gmail.com>
2020-09-29 00:09:56 +0200

    Uplaod&Update...

--- Udemy/The Modern Javascript Bootcamp Course (2020)/Secret Message Sharing App/app.js
+++ Udemy/The Modern Javascript Bootcamp Course (2020)/Secret Message Sharing App/app.js
@@ -1,10 +1,29 @@
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
 
-const secretMessageForm = document.querySelector('.card-panel form');
+if (message) {
+	const secretMessageShow = document.getElementById('message-show');
+	document.getElementById('message-form').classList.add("isHidden");
+	secretMessageShow.classList.remove("isHidden");
+	secretMessageShow.querySelector('h2').innerText = message;
+
+
+}
+
+const secretMessageForm = document.querySelector('.card-panel-1 form');
 
 secretMessageForm.addEventListener("submit", event => {
 	event.preventDefault();
 
-	const input = secretMessageForm.querySelector('input');
+	const input = secretMessageForm.querySelector('#message-input');
+	const encrypted = btoa(input.value);
 
-	console.log(input.value);
+	const linkInput = document.getElementById("link-input");
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
+	//document.execCommand("copy");
+
+	document.getElementById('message-form').classList.add("isHidden");
+	document.getElementById('link-form').classList.remove("isHidden");
+
 });
