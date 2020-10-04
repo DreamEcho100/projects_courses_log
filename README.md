7ea211fd0f726a62f183c158973a425d38a063c7
DreamEchoo100 <maze6572198@gmail.com>
2020-10-04 13:06:03 +0200

    Upload&Update...

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # "# projects_courses_log"
+
 &amp;
 The site is ready at https://dreamecho100.github.io/projects_courses_log/.
 &amp;
-## …or create a new repository on the command line
+
+## ï¿½or create a new repository on the command line
+
 echo "# projects_courses_log" >> README.md
 git init
 git add README.md
@@ -10,13 +13,25 @@ git commit -m "first commit"
 git branch -M master
 git remote add origin https://github.com/DreamEcho100/projects_courses_log.git
 git push -u origin master
-&amp;                
-## …or push an existing repository from the command line
+&amp;
+
+## ï¿½or push an existing repository from the command line
+
 git remote add origin https://github.com/DreamEcho100/projects_courses_log.git
 git branch -M master
 git push -u origin master
 
 ##
+
 git add .
 git commit -m "Uplaod&Update..."
 git push -u origin master
+
+&amp;
+
+## Running JS through command line
+
+npm init -y
+chmod +x index.js
+#!/usr/bin/env node
+npm link
--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/Tutorial2 - Reaching out to the Web (Http - Ajax)/containers/Blog/Blog.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/learning-react/src/Tutorial2 - Reaching out to the Web (Http - Ajax)/containers/Blog/Blog.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedId: null,
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatePosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Mazen",
+        };
+      });
+      this.setState({ posts: updatePosts });
+      console.log(response);
+    });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedId: id });
+  };
+
+  render() {
+    const posts = this.state.posts.map((post, index) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
