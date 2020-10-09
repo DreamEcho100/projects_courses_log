656a3630dad2f3cc68568df6d7371f1ee15be796
DreamEchoo100 <maze6572198@gmail.com>
2020-10-09 22:52:05 +0200

    Uplaod&Update...

--- Udemy/learning-react/src/App.js
+++ Udemy/learning-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import Tutorial1 from "./Tutorial1/Tutorial1";
 //import MyBurger from "./Project-1 - 1-The Burger Builder (Basic Version)/MyBurger";
-import MyBurger from "./Project-1 - 2-The Burger Builder (Accessing a Server)/MyBurger";
+import MyBurger from "./Project/2- The Burger Builder (Accessing a Server)/MyBurger";
 // import Tutorial2 from "./Tutorial2 - Reaching out to the Web (Http - Ajax)/Tutorial2";
 
 class App extends Component {
