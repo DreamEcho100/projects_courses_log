332d39b6191b1d136c64bba4a91c7be7263cd34f
DreamEchoo100 <maze6572198@gmail.com>
2020-10-27 16:43:30 +0200

    Uplaod&Update...

--- Youtube/Franks Laboratory/HTML5 Canvas Vanilla JavaScript Animation - Interactive Animated Background/script.js
+++ Youtube/Franks Laboratory/HTML5 Canvas Vanilla JavaScript Animation - Interactive Animated Background/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas1");
 const context = canvas.getContext('2d');
-context.imageSmoothingEnabled = false;
+//context.imageSmoothingEnabled = false;
 /*context.webkitImageSmoothingEnabled = false;
 context.mozImageSmoothingEnabled = false;
 context.msImageSmoothingEnabled = false;
@@ -12,17 +12,31 @@ let particalesArray = [];
 
 // Particale class
 class Particale {
-    constructor(x, y, dirextionX, dirextionY, size, color) {
+    constructor(moveRadius, step, position, size) {
+        this.moveRadius = moveRadius;
+        this.step = step;
+        this.position = position;
+        this.size = size;
     }
 
-    draw() {
+    draw(canvas) {
         context.beginPath();
-        context.fillStyle = this.color;
-        context.fill();
+        context.arc(
+            (Math.cos(this.position) * this.moveRadius) + (canvas.width / 2),
+            (Math.sin(this.position) * this.moveRadius) + (canvas.height / 2),
+            this.size,
+            0,
+            Math.PI * 2,
+            false
+        );
+        context.fillStyle = "white"; // this.color;
         context.closePath();
+        context.fill();
     }
 
     update(canvas) {
+        this.position += this.step;
+        this.draw(canvas);
     }
 }
 
@@ -30,24 +44,31 @@ function init(canvas) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     particalesArray = [];
-    const numberOfParticales = 0;
+    const numberOfParticales = Math.ceil((canvas.height * canvas.width) / 9000) * 4// 500;
     let i;
     for (i = 0; i < numberOfParticales; i++) {
-
-        particalesArray.push(new Particale());
+        const moveRadius = Math.random() * canvas.width;
+        const step = (Math.random() * 0.002) + 0.002;
+        const position = Math.random() * (Math.PI * 2);
+        const size = (Math.random() * 8) + 0.5;
+        
+        particalesArray.push(new Particale(moveRadius, step, position, size));
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.fillStyle = "rgba(0, 0, 0, 0.1)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.closePath();
     
     particalesArray.forEach(particale => particale.update(canvas));
 }
 
-// init(canvas);
-// animate();
+init(canvas);
+animate();
 
 window.addEventListener("resize", () => {
-    // init(canvas);
+    init(canvas);
 })
