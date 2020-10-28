59065e146bfbe9e24950e7a1d1da2ed38a815bc3
DreamEchoo100 <maze6572198@gmail.com>
2020-10-28 11:38:36 +0200

    Uplaod&Update...

--- Youtube/Franks Laboratory/Eye Balls Animation/script.js
+++ Youtube/Franks Laboratory/Eye Balls Animation/script.js
@@ -4,57 +4,107 @@ context.imageSmoothingEnabled = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let particalesArray = [];
+let eyes = [];
+let theta;
+
+const mouse = {
+    x: undefined,
+    y: undefined
+};
+
+canvas.addEventListener("mouseover", (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
 
 // Particale class
-class Particale {
-    constructor(x, y, size, color) {
+class Eye {
+    constructor(x, y, radius) {
         this.x = x;
         this.y = y;
-        this.size = size;
-        this.color = color;
+        this.radius = radius;
     }
 
-    draw(canvas) {
+    draw(mouse) {
+        // Draw eye
         context.beginPath();
-        context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        context.fillStyle = "white"; // this.color;
-        /*context.strokeStyle='rgba(255, 255, 255, 0.9)';
-        context.stroke();*/
-        context.closePath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = "red"; // this.color;
         context.fill();
+        context.closePath();
+        // Draw mouse
+        context.beginPath();
+        context.arc(mouse.x, mouse.y, 25, 0, Math.PI * 2, false);
+        context.fillStyle = "gold"; // this.color;
+        context.fill();
+        context.closePath();
     }
 
     update(canvas) {
-        this.draw(canvas);
+        this.draw(mouse);
     }
 }
 
 function init(canvas) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    particalesArray = [];
-    const numberOfParticales = Math.ceil((canvas.height * canvas.width) / 9000) * 3;// 500;
-    let i;
-    for (i = 0; i < numberOfParticales; i++) {
-        
-        particalesArray.push(new Particale(moveRadius, step, position, size, randomNumber));
+    eyes = [];
+
+    let overlapping = false;
+    const numberOfEyes = Math.ceil((canvas.height * canvas.width) / 9000) * 1.1;// 500;
+    const protection = 10000;
+    let counter = 0;
+    /*eyes.push(new Eye(
+        Math.random() * canvas.width,
+        Math.random() * canvas.height,
+        Math.floor(Math.random() * 100) + 5
+    ));*/
+    let eye, i, previousEye, directionX, directionY, distance;
+    while (eyes.length < numberOfEyes && counter < protection) {
+        eye = {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.floor(Math.random() * 100) + 5
+        }
+        overlapping = false;
+        for (i = 0; i < numberOfEyes; i++) {
+            previousEye = eyes[i];
+            if(previousEye) {
+                directionX = eye.x - previousEye.x;
+                directionY = eye.y - previousEye.y;
+            } else {
+                directionX = eye.x;
+                directionY = eye.y;
+                previousEye = {
+                    x: 0,
+                    y: 0,
+                    radius: 0
+                };
+            }
+            distance = Math.sqrt(directionX**2 + directionY**2);
+            if (distance < (eye.radius + previousEye.radius)) {
+                overlapping = true;
+            }
+        }
+        if (!overlapping) {
+            eyes.push(new Eye(eye.x, eye.y, eye.radius));
+        }
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
     context.beginPath();
-    context.fillStyle = "rgba(0, 0, 255, 0.1)";
+    context.fillStyle = "rgba(0, 0, 0, 0.1)";
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.closePath();
     
-    particalesArray.forEach(particale => particale.update(canvas));
+    eyes.forEach(eye => eye.draw(canvas));
 }
 
-// init(canvas);
-// animate();
+init(canvas);
+animate();
 
 window.addEventListener("resize", () => {
-    // init(canvas);
+    init(canvas);
 });
