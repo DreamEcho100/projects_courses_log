67df933d8651f07403143d0ab51209a69aea5b47
DreamEchoo100 <maze6572198@gmail.com>
2020-10-30 15:44:31 +0200

    Uplaod&Update...

--- main-content/Youtube/content.js
+++ main-content/Youtube/content.js
@@ -289,6 +289,20 @@ YotubeContent.list.push(
       },
       {
         types: ["normal-content-project"],
+        name: "Randomised Fractal Snowflakes",
+        info: "Randomised Fractal Snowflakes - Drawing Programmatic Snow JavaScript & HTML Canvas Element",
+        fileName: "index",
+        fileExtension: "html",
+        difficulty: "",
+        tech: {
+          languages: ["HTML", "CSS", "JavaScript"],
+          libraries: [],
+          frameworks: [],
+          APIs: [],
+        },
+      },
+      {
+        types: ["normal-content-project"],
         name: "Series 1 - Floating Particles Effect",
         info: "HTML Canvas & Vanilla JavaScript Animation Series 1 - Floating Particles Effect",
         fileName: "index",
