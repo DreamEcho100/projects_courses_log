9f891568d9c3551bca46c421d3345695f0e50967
DreamEchoo100 <maze6572198@gmail.com>
2020-10-31 19:06:50 +0200

    Uplaod&Update...

--- Youtube/Franks Laboratory/Liquid Particle Effect Around Any Element/script.js
+++ Youtube/Franks Laboratory/Liquid Particle Effect Around Any Element/script.js
@@ -1,3 +1,31 @@
+const liquidParticleEffectSection = document.getElementById("liquid-particle-effect-around-any-element");
 const canvas = document.getElementById("canvas1");
 const context = canvas.getContext('2d');
+
 context.imageSmoothingEnabled = false;
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+    x: null,
+    y: null
+}
+
+liquidParticleEffectSection.addEventListener("mousemove", (event) => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientX;
+});
+
+class Button {
+    constructor(x, y, width, height, baseX) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.baseX = baseX;
+    }
+    update() {
+        // 14:03
+    }
+}
