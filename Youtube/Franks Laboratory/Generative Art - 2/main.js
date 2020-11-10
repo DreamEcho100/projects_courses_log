770dafa8f88661a718ffdf6048c1f0a2ca9fd430
DreamEchoo100 <maze6572198@gmail.com>
2020-11-10 22:23:16 +0200

    Uplaod&Update...

--- Youtube/Franks Laboratory/Generative Art - 2/main.js
+++ Youtube/Franks Laboratory/Generative Art - 2/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const script = document.createElement("script")
-    script.src = "js/script2.js"// `js/${Math.random() >= 0.5 ? "script.js" : "script2.js"}`;
+    script.src = `js/${Math.random() >= 0.5 ? "script.js" : "script2.js"}`;
     document.body.appendChild(script);
 });
