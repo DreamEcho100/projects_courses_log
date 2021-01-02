b87d5fb34e2f503f4a7f1a67439908ba7ecd1f6c
DreamEchoo100 <maze6572198@gmail.com>
2021-01-03 01:08:40 +0200

    Uplaod&Update...

--- Udemy/The Complete Web Developer in 2021 Zero to Mastery/smartbrain-face-recognition/frontend/src/Components/Home/Home.js
+++ Udemy/The Complete Web Developer in 2021 Zero to Mastery/smartbrain-face-recognition/frontend/src/Components/Home/Home.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
-import Clarifai from 'clarifai';
 import Rank from "./Rank/Rank";
 import ImageLinkForm from "./ImageLinkForm/ImageLinkForm";
 import FaceDetiction from "./FaceDetiction/FaceDetiction";
-
-/*
-so you would change from in the video:
-
-  .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-
-to:
-
-  .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-*/
 
 class Home extends Component {
   constructor(props) {
@@ -82,25 +71,6 @@ class Home extends Component {
         },
         0
     );
-    /*
-    // -Face Detection model we will be using:
-    // https://www.clarifai.com/models/face-detection
-    // -How we will use the API with JS (don't worry I will show you the easy way to do this):
-    // https://docs.clarifai.com/api-guide/predict/images
-    // -Finally, this is the list of all the models you can use with this API if you want to customize your project:
-    // https://www.clarifai.com/model-gallery
-    app.models
-      .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-      */
   }
 
   render() {
@@ -118,89 +88,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-/*
-
-const app = new Clarifai.App({
-  apiKey: apikey
-});
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: "",
-      imageUrl: "",
-      boxes: [],
-      user: this.props.user
-    };
-  }
-
-  calculateFaceFunction = (data) => {
-    const clarifaiFace = data.region_info.bounding_box;
-    const image = document.getElementById("input-image");
-    const width = parseFloat(image.width);
-    const height = parseFloat(image.height);
-    
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),
-    };
-  }
-
-  displayFaceBoxes = (boxes) => {
-    this.setState({ boxes: boxes });
-  };
-
-  inputChangeHandler = (event) => {
-    this.setState({ input: event.target.value});
-  }
-
-  submitBtnHandler = () => {
-    this.setState({imageUrl: this.state.input});
-    // app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', "this.state.imageUrl")
-    setTimeout(
-      () => 
-        app.models.predict(Clarifai.FACE_DETECT_MODEL, (this.state.imageUrl))
-        .then(response => {
-          if (response) {
-            console.log(this.props.user.id);
-            fetch("https://floating-dusk-25989.herokuapp.com/image", {
-              method: "put",
-              headers: {"Content-Type": "application/json"},
-              body: JSON.stringify({ id: this.props.user.id })
-            })
-            .then(response => response.json())
-            .then(count => {
-              // const temObj = JSON.parse(JSON.stringify(this.props.user));
-              // temObj.entries = count;
-              // this.setState({ user: temObj });
-              const user = Object.assign(this.props.user, { entries: count });
-              this.setState({user: user});
-            });
-          }
-          this.displayFaceBoxes(response.outputs[0].data.regions.map(data => this.calculateFaceFunction(data)));
-        })
-        .catch(err => console.log(err)),
-      0
-    );
-  }
-
-  render() {
-    const { boxes, imageUrl } = this.state;
-    const { inputChangeHandler, submitBtnHandler } = this;
-
-    return (
-      <main>
-        <Rank name={this.props.user.name} entries={this.props.user.entries}/>
-        <ImageLinkForm onInputCange={inputChangeHandler} onSubmitBtnClick={submitBtnHandler} />
-        <FaceDetiction boxes={boxes} imageUrl={imageUrl} />
-      </main>
-    );
-  }
-}
-
-export default Home;
-*/
