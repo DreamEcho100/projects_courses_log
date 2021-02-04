b6b187bef52c483dc281dc00109ab13ef5d8af1c
DreamEcho100 <maze6572198@gmail.com>
2021-02-04 09:46:45 +0200

    Upload&Update...

--- Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/App.jsx
+++ Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home.component.jsx";
 import Shop from "./Pages/Shop/Shop.component.jsx";
+import SignInAndSignUp from "./Pages/SignInAndSignUp/SignInAndSignUp.component.jsx";
 import Header from "./Components/Header/Header.jsx";
 
 function App(props) {
@@ -12,6 +13,8 @@ function App(props) {
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route path="/shop" component={Shop} />
+					<Route exact path="/signin" component={SignInAndSignUp} />
+					<Route exact path="/signup" component={SignInAndSignUp} />
 				</Switch>
 			</main>
 		</>
---
+++ Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/Pages/SignInAndSignUp/SignInAndSignUp.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInAndSignUp = () => {
+	return (
+		<section>
+			<form></form>
+		</section>
+	);
+};
+
+export default SignInAndSignUp;
