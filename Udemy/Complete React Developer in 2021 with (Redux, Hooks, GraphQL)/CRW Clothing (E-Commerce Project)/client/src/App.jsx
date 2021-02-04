c174d92d2228eb10301aff847f2d0564f305e188
DreamEcho100 <maze6572198@gmail.com>
2021-02-04 09:30:37 +0200

    Upload&Update...

--- Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/App.jsx
+++ Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import { Route, Switch, NavLink, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home.component.jsx";
-import Shop from "./Pages/Shop/Shop.component";
+import Shop from "./Pages/Shop/Shop.component.jsx";
+import Header from "./Components/Header/Header.jsx";
 
 function App(props) {
 	return (
 		<>
+			<Header />
 			<main>
 				<Switch>
 					<Route exact path="/" component={Home} />
--- Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/Components/Header/Header.jsx
+++ Udemy/Complete React Developer in 2021 with (Redux, Hooks, GraphQL)/CRW Clothing (E-Commerce Project)/client/src/Components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assests/crown.svg";
 import "./Header.styles.scss";
 
 const Header = () => {
 	return (
-		<Header>
-			<nav></nav>
-		</Header>
+		<header className="main-header">
+			<NavLink className="logo-container" activeClassName="active-link" to="/">
+				<Logo className="logo" />
+			</NavLink>
+			<nav className="options">
+				<NavLink className="option" activeClassName="active-link" to="/shop">
+					SHOP
+				</NavLink>
+				<NavLink className="option" activeClassName="active-link" to="/contact">
+					CONTACT
+				</NavLink>
+			</nav>
+		</header>
 	);
 };
 
