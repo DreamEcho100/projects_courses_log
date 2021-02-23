32b627bc8a20aa71b158a7f9b9b4e7800b4ea4ef
DreamEcho100 <maze6572198@gmail.com>
2021-02-23 02:59:53 +0200

    Uplaod& ee

--- main-content/Udemy/content.js
+++ main-content/Udemy/content.js
@@ -54,7 +54,21 @@ UdemyContent.list.push({
 		{
 			types: ['normal-content-project'],
 			name: 'Expanding Cards',
-			info: 'Expanding Cards',
+			info: '',
+			fileName: 'index',
+			fileExtension: 'html',
+			difficulty: '',
+			tech: {
+				languages: ['HTML', 'CSS', 'JavaScript'],
+				libraries: [],
+				frameworks: [],
+				APIs: [],
+			},
+		},
+		{
+			types: ['normal-content-project'],
+			name: 'Progress Steps',
+			info: '',
 			fileName: 'index',
 			fileExtension: 'html',
 			difficulty: '',
